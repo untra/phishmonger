5e002a05ad1f970943e02b6c492b6efc6438637d
untra <untra.sam@gmail.com>
2015-10-24 14:18:29 -0600

    model objects are setup.

---
+++ src/models/target.py
@@ -0,0 +1,12 @@
+from basemodel import BaseModel
+class Target(BaseModel):
+    def requiredFields():
+        super + ['fname', 'lname', 'email', 'group']
+
+    def fields():
+        super.update({
+            'fname' : (is_string, ),
+            'lname' : (is_string, ),
+            'email' : (is_string, ),
+            'group' : (is_string, ),
+        })
--- src/server.py
+++ src/server.py
@@ -9,13 +9,16 @@ from tornado import ioloop, gen
 from tornado.concurrent import Future, chain_future
 from tornado.options import define, options
 from config.config import application
+import models.driver as driver
 
 def main():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
     print "Listening for connections on... localhost:{0}".format(options.port)
+    driver.init()
     tornado.ioloop.IOLoop.instance().start()
+
 
 if __name__ == "__main__":
     main()
